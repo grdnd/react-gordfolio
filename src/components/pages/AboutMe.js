f815e57344c64eae47b2808d3a1682b17edbf999
Gordo <gordonhdo@gmail.com>
2022-05-13 22:28:52 -0700

    creating pages

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../App.css";
+
+export default function AboutMe() {
+  return <h1 className="about-me">About Me</h1>;
+}
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../App.css";
+
+export default function Resume() {
+  return <h1 className="resume">Resume</h1>;
+}
