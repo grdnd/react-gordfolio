5b41b69436535fea06daabc75d610251053c84e4
Gordo <gordonhdo@gmail.com>
2022-05-16 16:48:25 -0700

    portfolio page done

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../../App.css";
 import Cards from "../Cards";
-import Footer from "../Footer";
 
 function Portfolio() {
   return (
     <>
+      <div className="portfolio">
+        <h1 className="title">Portfolio</h1>
+      </div>
       <Cards />
-      <Footer />
     </>
   );
 }
