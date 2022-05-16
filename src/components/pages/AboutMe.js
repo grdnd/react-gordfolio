61377efd716300a9d05b7b02630d2b2b504f3dec
Gordo <gordonhdo@gmail.com>
2022-05-16 16:48:04 -0700

    about me page done

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -9,7 +9,33 @@ export default function AboutMe() {
         <h1 className="title mt-0 mb-0" id="abt-me">
           About Me
         </h1>
-        <BioCard />
+      </div>
+      <BioCard />
+      <div className="about-me-text-container is-flex is-justify-content-center is-align-items-center mt-0 mb-0">
+        <div className="about-me-text-wrapper is-flex is-justify-content-center is-align-items-center">
+          <div className="about-me-text-box">
+            <div className="about-me-text-title-container">
+              <h1>Expression Over Impression</h1>
+            </div>
+            <div className="about-me-text-content">
+              <p>
+                I am an aspiring web developer based in Los Angeles and Orange
+                County, California.
+                <br></br>
+                <br></br>Throughout my lifetime I have been infatuated with art
+                of all forms, and have seen firsthand the many ways
+                self-expression propels individuals to become the greatest
+                versions of themselves. With my pursuit of knowledge in web
+                development skills,
+                <br></br>
+                <span>
+                  I wish to accomplish my dream of helping creators bring their
+                  visions come to life.
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
