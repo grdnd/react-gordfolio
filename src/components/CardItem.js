868b492bdb4c4b255fb167b773d3b3a16b6a12f2
Gordo <gordonhdo@gmail.com>
2022-05-17 23:22:08 -0700

    edit card item component to fit external links

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,15 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function CardItem(props) {
   return (
     <>
       <li className="cards__item">
-        <Link
-          className="cards__item__link"
-          to={{ pathname: "https://github.com/grdnd" }}
-          target="_blank"
-        >
+        <a className="cards__item__link" href={props.href} target="_blank">
           <figure className="cards__item__pic-wrap" data-category={props.label}>
             <img src={props.src} alt="" className="cards__item__img" />
             <h1 className="cards__item__hover__text"></h1>
@@ -17,7 +13,7 @@ function CardItem(props) {
           <div className="cards__item__info">
             <h5 className="cards__item__text">{props.text}</h5>
           </div>
-        </Link>
+        </a>
       </li>
     </>
   );
