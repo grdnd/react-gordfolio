324a719668bf989be5cb8d147d3d99e091ed273d
Gordo <gordonhdo@gmail.com>
2022-05-22 22:56:59 -0700

    fixing resume columns

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -14,35 +14,35 @@ export default function Resume() {
                   <h2>&#125; Front-end Proficiencies &#123;</h2>
                 </li>
                 {/* <hr /> */}
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-html5-plain mobile-icon" />
                   <p>HTML</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-css3-plain mobile-icon" />
                   <p>CSS</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-javascript-plain mobile-icon" />
                   <p>JavaScript</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-jquery-plain mobile-icon" />
                   <p>jquery</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-react-original mobile-icon" />
                   <p>React</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-bootstrap-plain mobile-icon" />
                   <p>bootstrap</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-bulma-plain mobile-icon" />
                   <p>Bulma</p>
                 </li>
-                <li>Responsive Design</li>
+                <li className="skill-icon">Responsive Design</li>
               </ul>
             </div>
             <aside className="divider-container is-flex is-justify-content-center">
@@ -51,36 +51,38 @@ export default function Resume() {
             <div className="column is-half mt-0 is-flex is-flex-direction-column is-align-items-center">
               <ul className="skills">
                 <li>
-                  <h2>&#123; Back-end Proficiencies &#125;</h2>
+                  <h2 className="backend">
+                    &#123; Back-end Proficiencies &#125;
+                  </h2>
                 </li>
                 {/* <hr /> */}
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-nodejs-plain mobile-icon" />
                   <p>Node.js</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-express-original mobile-icon" />
                   <p>express</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-sequelize-plain mobile-icon" />
                   <p>Sequelize</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-mysql-plain mobile-icon" />
                   <p>MySQL</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-mongodb-plain mobile-icon" />
                   <p>MongoDB</p>
                 </li>
-                <li className="is-flex is-align-items-center is-flex-direction-column">
+                <li className="skill-icon is-flex is-align-items-center is-flex-direction-column">
                   <i className="devicon-graphql-plain mobile-icon" />
                   <p>GraphQL</p>
                 </li>
-                <li>Mongoose</li>
-                <li>REST</li>
-                <li>APIs</li>
+                <li className="skill-icon">Mongoose</li>
+                <li className="skill-icon">REST</li>
+                <li className="skill-icon">APIs</li>
               </ul>
             </div>
           </div>
